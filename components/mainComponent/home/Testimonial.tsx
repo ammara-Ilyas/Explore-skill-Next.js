ba8186fee05a6648cb7f849e2af1f6561addfc7c
ammara-Ilyas <ismarailyas545@gmail.com>
2024-05-12 01:32:24 +0500

    Key prop

--- components/mainComponent/home/Testimonial.tsx
+++ components/mainComponent/home/Testimonial.tsx
@@ -23,8 +23,11 @@ function Testimonial() {
       <Para text="Lorem ipsum dolor, sit amet consectetur adipisicing elit." />
       <div className=" lg:10/12  md:w-11/12 w-11/12 mx-auto flex flex-wrap justify-center gap-8 lg:gap-16 pt-4 items-center pb-6">
         {data.map((item, i) => (
-          <div className="rounded-lg duration-300 hover:shadow-primary group-hover:shadow-primary sm:w-[45%] w-3/4  xl:w-[45%] flex justify-center p-5 gap-5 font-extralight  bg-rose-50">
-            <div className=" w-[12%] " key={i}>
+          <div
+            className="rounded-lg duration-300 hover:shadow-primary group-hover:shadow-primary sm:w-[45%] w-3/4  xl:w-[45%] flex justify-center p-5 gap-5 font-extralight  bg-rose-50"
+            key={i}
+          >
+            <div className=" w-[12%] ">
               <Image
                 src={user}
                 alt="campus image"
